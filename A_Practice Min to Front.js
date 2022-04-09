ef4ad17d7cfaba0987f785c64ad7a564bfae7cda
Lisa <lisa.hl.chen@gmail.com>
2022-04-09 14:16:01 -0700

    Practice Assignment

--- A_Practice Min to Front.js
+++ A_Practice Min to Front.js
@@ -1,26 +1,26 @@
 //Given an array of comparable values, move the lowest element to array’s front, shifting backward any elements previously ahead of it. Do not otherwise change the array’s order. Given [4,2,1,3,5], change it to [1,4,2,3,5] and return it. As always, do this without using built-in functions.
 
-// [4,2,1,3,5]
-// [1,4,2,3,5]
-
 function minToFront(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        min = arr[0];
+    var min = arr[0];
+    var minPosition = 0;
+    
+    for (let i = 1; i < arr.length; i++){
         
-        
-        
-        // var min = arr[0];
-        // var newArray = [];
-        // if (arr[i] < min) {
-        //     min = arr[i];
-        //     newArray[0] = min;
-        // }
-        // else {
-        // newArray.push(arr[i]);
-        // }
+        if (arr[i] < min) {            
+            minPosition = i;
+            min = arr[minPosition];   
+        }
     }
-    return newArray
-}
 
+    for (let j = minPosition; j > 0; j--) {
+
+        var temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+    }
+    return arr;
+}
+// INPUT [4,2,1,3,5]
+// OUTPUT [1,4,2,3,5]
 var Solution = minToFront([4, 2, 1, 3, 5]);
 console.log("Solution is ", Solution);
